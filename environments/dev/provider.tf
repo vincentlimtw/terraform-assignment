b20c1705be77b1367fe09abdddd5d4d2b2a877f6
Vincent Lim <vincent.lim@thoughtworks.com>
2026-03-23 17:17:06 +0800

    Add Time provider and update AWS provider version

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "6.37.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.13.1"
     }
   }
 }
